657b098e0d6598de7941ceae49e48bb4f4ed08ad
Lionel jevon <lioneljevon1212@gmail.com>
2023-09-12 20:07:58 +0700

    Video 2
    Variabel dan Konstanta

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Belajar Javascript");
+// Variabel dan konstanta
+
+
+let angka; //Variabel angka
+let string = "Halo"; //Variabel String
+
+angka = 10; //asign variabel 'angka'
+
+// Konstanta (Tidak bisa diubah)
+const phi = 3.14;
+// phi = 5; (Error)
+
+console.log(angka);
+console.log(string);
+console.log(phi);
+angka = 5;
+console.log(angka); //Hasil akan menjadi 5
+
