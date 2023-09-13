e57d69b977e8c17066b20ddefa5e25c18d010a5b
Lionel jevon <lioneljevon1212@gmail.com>
2023-09-13 22:49:34 +0700

    Video 5
    Operator logika

--- script.js
+++ script.js
@@ -1,64 +1,28 @@
-//Operator Matematika
+//Operator Logika
 
-//operator unary
-let x = 12;
-x = -x; //menghasilkan angka -12
-console.log(x);
+//inisialisasi
+let a = true;
+let b = false;
+let c = true;
+let d = false;
 
-//operator binary
+//operator OR '||' jika salah satu bernilai true maka hasilnya adalah true
+console.log(a || b); //menghasilkan true karna salah satu bernilai true
+console.log(a || c); //menghasilkan false
+
+//operat VND '&&' jika semua operand true maka hasilnya true
+console.log(a && c); //menghasilkan true
+console.log(a && b); //menghasilkan false karna salah satu operand bernilai false
+
+//Operator Pembanding 
+let x = 1;
 let y = 2;
-let z = 1;
-console.log(y-z); //Operator binary Membutuhkan 2 atau lebih operand
+console.log(x>y); //operator lebih besar tapi ini false
+console.log(x<y); //operator lebih kecil
+console.log(x<=y); //operator lebih kecil sama dengan
+console.log(x>=y); //operator lebih besar sama dengan
 
-//Operator aritmatika
-console.log(2+2); //Pertambahan
-console.log(3-2); //Pengurangan
-console.log(3*2); //Perkalian
-console.log(4/2); //Pembagian
-console.log(5%2); //Modulo. Modulo adalah hasil sisa dari pembagian integer
-console.log(2**3); //Eksponen atau kuadrat disitu 2 dikuadratkan 3
-
-//Mencoba rumus
-let p = 2,
-l = 3,
-t = 7,volB; //variabel untuk menghitung volume balok
-volB = p * l * t;
-console.log(volB);
-
-let a = 2,
- tS = 6,
- luasSegitiga;
-luasSegitiga = (1/2) * a * tS;
-console.log(luasSegitiga); //Luas Segitiga
-
-//Operator string 
-let pace = "OPM";
-let indo = "Indonesia";
-console.log(pace + " Love " + indo);
-
-//angka string dsb
-console.log('1' + 2); //akan menghasilkan 12 bukan 3 karna 1 adalah string
-console.log(3 + 3 + '2'); //akan menghasilkan 62;
-console.log('1' + 2 + 3); //akan menghasilkan 123 karna operand pertama adalah string
-console.log(5 - "2"); //akan menghasilkan 3 karna 2 akan dikonversi otomatis
-console.log(10 / "2"); //sama operand 2 akan dikonversi sehingga hasil = 5
-
-//operator precendance 
-console.log(2 + 2 * 3); //8 karna pekalian diutamakam
-console.log((2 + 2) * 3); //12 karna tanda kurung lebih diutamakan
-
-//assigment
-let var1 =5;
-let var2 = 7;
-var2 = var1; //menghasilkan 5
-var1 = var2; //tetap 5 karna b sudah di asign
-
-//increment dan decrement
-let inc = 1;
-console.log(inc++); //HVsil =2
-let inc2 = 1;
-inc2 = inc2 +1; //Logikanya
-
-let dec = 2;
-console.log(dec--); //Hasil = 1
-
+ let i = "5";
+ let j = 5;
+ console.log(i == j); //operator sama dengan hasilnya true
+ console.log(i === j); //false meskipun nilai sama tapi pengecekan ini juga sampai tipe data
