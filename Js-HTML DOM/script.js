bd760aef6e59dfa531e232d51a34a713ff6507c1
Lionel jevon <lioneljevon1212@gmail.com>
2023-09-25 13:57:52 +0700

    Video 13
    Js dom

---
+++ Js-HTML DOM/script.js
@@ -0,0 +1,7 @@
+alert("Test Javascript"); 
+//Fungsi document.getElementById mengambil id di elemen html
+document.getElementById("target1").innerHTML = "Tulisan ini berubah";
+document.getElementById("target2").innerText ="lorem ipsum dolor mit";
+//query digunakan untuk mengambil elemen
+document.querySelector("h1").innerText = "Query";
+document.querySelector("#isikonten").innerText = "Isi konten dari container";
