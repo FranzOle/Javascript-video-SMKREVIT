d2096fefa6a5bc90f6da39e7314855cfa23169a7
Lionel jevon <lioneljevon1212@gmail.com>
2023-10-26 13:48:06 +0700

    Video 17
    Document Query Selector All

--- Js-HTML DOM/script.js
+++ Js-HTML DOM/script.js
@@ -1,80 +1,23 @@
-alert("Test Javascript"); 
-//Fungsi document.getElementById mengambil id di elemen html
-document.getElementById("target1").innerHTML = "Tulisan ini berubah";
-document.getElementById("target2").innerText ="lorem ipsum dolor mit";
-//query digunakan untuk mengambil elemen
-document.querySelector("h1").innerText = "Query";
-document.querySelector("#isikonten").innerText = "Isi konten dari container";
+//video 17 document.queryselectorall
+//selectorall digunakan untuk menganbil semua tag atau selector yg terpanggil
 
-//Video 14 Event Dom js 
-//metode 1 menggunakan onclick di elemen HTML 
-function TesMetode(){
-    document.querySelector("#berubah").innerText = "Berubah";
+let btn = document.querySelectorAll(".containerangka > button");
+let btn2 = document.querySelectorAll(".containermat > button");
+console.log(btn2);
+console.log(btn);
+
+//Variabel btn dan btn2 akan menjadi array(Nodelist) karna btn mengambbil semua atribut btutton
+//cara lain menampilkan nodelist
+for(let i=0; i<btn.length; i++){
+    console.log(btn[i]);
 }
 
-//metode 2 menggunakan function anonymous
-klik.onclick = function (){
-   let objFont = document.getElementById("berubahid");
-   objFont.innerHTML = "Berubah-berubah id";
-   objFont.style.color = "red";
+//menampilkan angka inputan
+let tampil = document.querySelector("#tampil");
+console.log(btn[7].innerHTML);
+
+for(let index = 0; index < btn.length; index++){
+    btn[index].onclick = function(){
+        tampil.value = btn[index].innerHTML;
+    };
 }
-
-//metode 3 menggunakan event listener
-let fungsi = document.querySelector("#klikk");
-fungsi.addEventListener("click",def);
-
-function def (){
-    let objFun = document.getElementById("berubahlistener");
-    objFun.innerText = "ini menggunakan Listener";
-}
-
-function tampil (){
-    let objfun = document.querySelector("#rubah");
-    objfun.innerText += "Lol"
-}
-
-function tampil2(){
-    let objfunn = document.querySelector("#rubah2");
-    objfunn.innerText = "Berubah";
-    objfunn.style.color = "blue";
-}
-
-function Fungsievent (p){
-    let a = document.querySelector("#rubah3").innerText = "Belajar JS " + p;
-    console.log("Tes");
-}
-
-//video 15 EventListener
-
-const kiklik = document.querySelector("#klikaku");
-const mouse = document.querySelector("#mouseaku");
-
-mouse.addEventListener("mouseover",MouseAku); //menggunakan metode mouseover
-mouse.addEventListener("mouseout", function (){
-    const div = document.querySelector(".container-listener");
-    div.innerHTML += "P ";
-}); //langsung memanggil function
-
-function MouseAku(){
-    const div = document.querySelector(".container-listener");
-    div.innerHTML += "Hi ";
-}
-
-kiklik.addEventListener("click",python);
-
-function python(){
-    const div = document.querySelector(".container-listener");
-    div.innerHTML = "Hi klik";
-}
-
-coba.onclick = python;  
-
-coba.onmouseover = function(){
-    let coba = document.querySelector("#coba");
-    coba.style.color = "red";
-};
-
-coba.onmouseout = function(){
-    let coba = document.querySelector("#coba");
-    coba.style.color = "black";
-};
--- Projek HTML-js/script.js
+++ Projek HTML-js/script.js
@@ -3,7 +3,7 @@ const tampilan = document.querySelector("#tampil");
 naik.onclick = function(){
     i++;
     tampilan.innerHTML = i;
-    tampilan.style.color = "blue";
+    tampilan.style.color = "#9fb4f2";
     if(i == 0){
         tampilan.style.color = "black";
     }
@@ -13,7 +13,7 @@ turun.onclick = function(){
     i--;
     if(i >= 0){
     tampilan.innerHTML = i;
-    tampilan.style.color = "red";
+    tampilan.style.color = "#d31b1b";
     if(i == 0){
         tampilan.style.color = "black";
     }}
