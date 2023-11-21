9b987e85bdc5f8de4c6169a52e26eac3299476e0
Lionel jevon <lioneljevon1212@gmail.com>
2023-11-21 07:47:15 +0700

    Video 19
    array methods

--- Js-HTML DOM/script.js
+++ Js-HTML DOM/script.js
@@ -1,23 +1,29 @@
-//video 17 document.queryselectorall
-//selectorall digunakan untuk menganbil semua tag atau selector yg terpanggil
+//Video 19 array methods
+let p = document.querySelectorAll("p");
+let nilai =[
+    {nama:"reno", ipa:90, ips:80, rpl:90},
+    {nama:"bondi", ipa:30, ips:70, rpl:60},
+    {nama:"budi", ipa:92, ips:84, rpl:95},
+    {nama:"doni", ipa:95, ips:80, rpl:100},
+]
 
-let btn = document.querySelectorAll(".containerangka > button");
-let btn2 = document.querySelectorAll(".containermat > button");
-console.log(btn2);
-console.log(btn);
+let nama = ["reno","bondi","budi", "doni" ];
 
-//Variabel btn dan btn2 akan menjadi array(Nodelist) karna btn mengambbil semua atribut btutton
-//cara lain menampilkan nodelist
-for(let i=0; i<btn.length; i++){
-    console.log(btn[i]);
-}
+console.log(nilai);
+console.log(nama);
+console.log(nilai[0].nama);
+nama.push("rudi");
+nama.push("leni") //berfungsi menambahkan data setelah data terakhir
+// delete nama[0]; //berfungsi untuk menghapus data array sesuai index
+// nama.splice(1,1);
+nama.unshift("tejo", "boni"); //menambahkan elemen diawal
+p[0].innerHTML = nama;
+// p.innerHTML = nilai[0].nama;
+// p.innerHTML += " " + nilai[0].ipa;
+// p.innerHTML = nama.pop(); //mengambil data terakhir array
+// nama.splice(0, 1);
+nama.splice(1,3); //menghapus pilihan array sesuai index dan berapa elemen
+p[1].innerHTML = " " + nama;
 
-//menampilkan angka inputan
-let tampil = document.querySelector("#tampil");
-console.log(btn[7].innerHTML);
-
-for(let index = 0; index < btn.length; index++){
-    btn[index].onclick = function(){
-        tampil.value = btn[index].innerHTML;
-    };
-}
+let abjad = ["a", "b", "c", "d", "e", "f","g"];
+p[2].innerHTML = abjad.slice(0,3); //slice abjad digunakan itu mengkeep elemen sesuai index
