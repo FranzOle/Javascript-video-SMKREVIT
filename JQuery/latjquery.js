d4569a081a1e47bd4cef95227cddcf432b8b3afb
Lionel jevon <lioneljevon1212@gmail.com>
2023-11-23 13:59:38 +0700

    Video 32
    JQuery Event

--- JQuery/latjquery.js
+++ JQuery/latjquery.js
@@ -1,8 +1,17 @@
+//video 32
+
 $(document).ready(function () {
     console.log("ppp");
 });
 
 $(function () {
     $("#isi").html("<h1>Belajar Jquery</h1>")
-    
+    $("#klik").click(function (e) { 
+        e.preventDefault();
+        alert("belajar javascript");
+    });
+    $("#isi").mouseleave(function () { 
+        alert("mouse leave");
+        console.log("mouse");
+    });
 });
